fee105663e51a0708794591b193280516c167a1e
Bethany Bin <bethanybin@bethanys-mbp-4.attlocal.net>
2018-02-11 16:10:28 -0800

    similar movies done

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Bethany Bin on 2/6/18.
+//  Copyright © 2018 Bethany Bin. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
